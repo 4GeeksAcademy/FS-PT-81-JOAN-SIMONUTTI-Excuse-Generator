d5dab00a417c483ed62d3fb983d1a60facbdefa0
Joan <soyjoansimonutti@gmail.com>
2024-10-24 11:59:33 +0000

    New Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,34 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// Función que genera una excusa aleatoria
+function generateExcuse() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  // Seleccionar un elemento aleatorio de cada array
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
 
+  // Concatenar la excusa
+  return `${randomWho} ${randomAction} ${randomWhat} ${randomWhen}.`;
+}
+
+// Función que actualiza el contenido del elemento #excuse al cargar la página
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateExcuse();
 };
+
+// Evento que escucha el clic del botón para generar una nueva excusa
+document
+  .getElementById("generateExcuseButton")
+  .addEventListener("click", function() {
+    document.getElementById("excuse").innerHTML = generateExcuse();
+  });
